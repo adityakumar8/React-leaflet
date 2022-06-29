e329dd5b57b0177b8169bcb725c4dad743ac84e0
Aditya Kumar <aditya.kumar@indianexpress.com>
2022-06-29 15:47:46 +0530

    initial commit

---
+++ src/components/MyMap.jsx
@@ -0,0 +1,10 @@
+import React,{ Component } from "react";
+
+class MyMap extends Component {
+    state = { }
+    render() {
+        return (<div><h1>My Map</h1></div>);
+    }
+}
+
+export default MyMap;
