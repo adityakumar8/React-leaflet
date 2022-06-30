4a3d287d396a2a964acf97eca17502cadf58b90a
Aditya Kumar <aditya.kumar@indianexpress.com>
2022-06-30 10:26:47 +0530

    popup country name added

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,7 +1,8 @@
 import React,{ Component } from "react";
 import { MapContainer ,GeoJSON } from "react-leaflet";
 import mapData from './../data/countries.json';
-import "leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css";
+import "./MyMap.css";
 
 
 class MyMap extends Component {
@@ -11,12 +12,37 @@ class MyMap extends Component {
     componentDidMount() {
       console.log(mapData);
     }
+
+    countryStyle = {
+        fillColor: "blue",
+        fillOpacity: 1,
+        color: "black",
+        weight: 2,
+        // dashArray: 5,
+    };
+
+    onEachCountry = (country, layer) => {
+      const countryName = country.properties.ADMIN;
+      console.log(countryName);
+      layer.bindPopup(countryName);
+      
+      layer.on({
+        mouseover: (event)=> {
+          event.target.setStyle({
+            color: "green",
+            fillColor: "yellow",
+            fillOpacity: 0.5,
+          });
+        }
+      })
+    }
+
     render(){
     return(
     <div>
     <h1 style={{textAlign: "center"}}> MyMap</h1>
-    <MapContainer style= {{height:"80vh"}} zoom={2} center={[20, 100]}>
-        <GeoJSON data={mapData.features}/>
+    <MapContainer style= {{height:"80vh"}} zoom={2} center={[20, 40]}>
+        <GeoJSON style={this.countryStyle} data={mapData.features} onEachFeature={this.onEachCountry}/>
 
     </MapContainer>
     
